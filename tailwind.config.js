922e8aa58192e1c6c51dba747a7dcdc0e8fb9a39
k-ravi24 <k.ravi19777@gmail.com>
2024-07-27 17:23:35 +0530

    added: Screen resolutions

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "small-mobile": "480px",
+        mobile: "640px",
+        "small-tablet": "768px",
+        tablet: "1024px",
+        laptop: "1369px",
+        desktop: "1920px",
+      },
+    },
   },
   plugins: [],
 };
